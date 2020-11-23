80e6e70acdcf6097a7815dcda3a02c2c2346089c
Cameron <camjw119@gmail.com>
2020-11-23 20:00:33 +0000

    update scene

--- src/scripts/scenes/first_scene.cpp
+++ src/scripts/scenes/first_scene.cpp
@@ -1,8 +1,8 @@
 #include "first_scene.h"
 #include <maths/transform.h>
 #include <rendering/point_light.h>
-#include <utils/opengl_helpers.h>
 #include <utility>
+#include <utils/opengl_helpers.h>
 
 FirstScene::FirstScene(std::shared_ptr<DemoContext> context, const std::shared_ptr<World>& world, SceneID scene_id)
     : Scene(std::move(context), world, scene_id)
@@ -15,7 +15,7 @@ void FirstScene::on_create()
     build_camera();
     build_skybox();
 
-    context->get_model_importer()->load_file("../assets/models/crytek_sponza.fbx", graph->root().get());
+    context->get_model_importer()->load_file("../assets/models/Sponza/sponza.gltf", graph->root().get());
 }
 
 void FirstScene::on_destroy()
@@ -34,6 +34,73 @@ void FirstScene::build_camera()
                      .position = float3(-1.0, 0.0, 0.0),
                  })
                  ->build();
+
+    world->create_entity()
+        ->with(DirectionalLight {
+            .direction = float3(0.0, -5.0, 1.333).normalise(),
+            .ambient = float3(1.0, 1.0, 1.0),
+        })
+        ->build();
+
+    world->create_entity()
+        ->with(PointLight {
+            .colour = float3(1.0, 0.0, 0.0),
+            .constant = 0,
+            .linear = 1,
+            .quadratic = 0,
+            .ambient = float3(1.0, 0.0, 0.0),
+            .diffuse = float3(1.0, 0.0, 0.0),
+            .specular = float3(1.0, 0.0, 0.0),
+        })
+        ->with (Transform {
+            .position = float3(-110.0, 14.0, -41.0),
+        })
+        ->build();
+
+    world->create_entity()
+        ->with(PointLight {
+            .colour = float3(0.0, 1.0, 0.0),
+            .constant = 0,
+            .linear = 1,
+            .quadratic = 0,
+            .ambient = float3(0.0, 1.0, 0.0),
+            .diffuse = float3(0.0, 1.0, 0.0),
+            .specular = float3(0.0, 1.0, 0.0),
+        })
+        ->with (Transform {
+            .position = float3(-110.0, 14.0, -41.0),
+        })
+        ->build();
+
+    world->create_entity()
+        ->with(PointLight {
+            .colour = float3(0.0, 0.0, 1.0),
+            .constant = 0,
+            .linear = 1,
+            .quadratic = 0,
+            .ambient = float3(0.0, 0.0, 1.0),
+            .diffuse = float3(0.0, 0.0, 1.0),
+            .specular = float3(0.0, 0.0, 1.0),
+        })
+        ->with (Transform {
+            .position = float3(110, 16.0, 41.0),
+        })
+        ->build();
+
+    world->create_entity()
+        ->with(PointLight {
+            .colour = float3(1.0, 0.0, 1.0),
+            .constant = 0,
+            .linear = 1,
+            .quadratic = 0,
+            .ambient = float3(1.0, 0.0, 1.0),
+            .diffuse = float3(1.0, 0.0, 1.0),
+            .specular = float3(1.0, 0.0, 1.0),
+        })
+        ->with (Transform {
+            .position = float3(-110.0, 16.0, 41.0),
+        })
+        ->build();
 }
 
 void FirstScene::build_skybox()
